30f73469cd9a40471426d2e96f7c6c62ba58af63
Ad van der Veer <advanderveer@gmail.com>
2013-11-29 16:41:53 +0100

    first release, created resolver based on freight.js service container

--- src/shared/resolver.js
+++ src/shared/resolver.js
@@ -1,7 +1,94 @@
-var ServiceResolver = function() {
+var Resolver = require('ticket.js/src/resolver.js');
+var ServiceResolver = function(container) {
+  var self = this;
+  var parent = ServiceResolver.prototype;
 
+  var getControllerAttribute = function(transit) {
+    if(transit.hasAttribute('_controller')) {
+      return transit.getAttribute('_controller');
+    }
+    return false;
+  };
+
+  var getControllerService = function(str) {
+    var args = str.split(self.ACTION_SEPERATOR);
+    var ids = container.findTaggedServiceIds(self.CONTROLLER_TAG);
+    var id = args[0];
+
+    if(ids.indexOf(id) === -1)
+      throw new Error('Could not find an controller named: "'+str+'", registered services under tag "'+self.CONTROLLER_TAG+'": [' + ids + ']');
+
+    var controller = container.get(id);
+    var action = args[1];
+
+    if(typeof controller[action] !== 'function') {
+      throw new Error('The provided controller "'+id+'" does not have function named: "'+action+'"');
+    }
+
+    return {
+      scope: controller,
+      fn: controller[action] 
+    };
+  };
+
+  /**
+   * The container used for resolving
+   * 
+   * @type {Freight}
+   */
+  self.container = container;
+
+  /**
+   * The tag that indicates controller services
+   * 
+   * @type {String}
+   */
+  self.ACTION_SEPERATOR = ':';
+
+  /**
+   * The tag that indicates controller services
+   * 
+   * @type {String}
+   */
+  self.CONTROLLER_TAG = 'controller';
+
+  /**
+   * Get the controller object from the dependency container
+   * 
+   * @param  {Transit} transit the transit
+   * @return {Object}  the object (scope)
+   */
+  self.getScope = function getScope(transit) {
+
+    var str = getControllerAttribute(transit);
+    if(typeof str !== 'string')
+      return parent.getScope(transit);
+
+    var res = getControllerService(str);
+    return res.scope;
+  };
+
+  /**
+   * Get the function from the dependency container
+   * 
+   * @param  {Transit} transit the transit
+   * @return {Function} the function
+   */
+  self.getFunction = function getFunction(transit) {
+    var str = getControllerAttribute(transit);
+    if(typeof str !== 'string')
+      return parent.getFunction(transit);
+
+    var res = getControllerService(str);
+    return res.fn;
+  };
 
 
 };
+
+
+//extends basic resolver
+ServiceResolver.prototype = new Resolver();
+ServiceResolver.prototype.constructor = ServiceResolver; 
 
 module.exports = ServiceResolver;
--- test/shared/resolver_test.js
+++ test/shared/resolver_test.js
@@ -1,16 +1,100 @@
 var ServiceResolver = require('../../src/shared/resolver.js');
+var Freight = require('freight.js');
+var Transit = require('ticket.js/src/transit.js');
 
 describe('Collection', function(){
 
-  var r, f;
+  var defaultController = {
+    index: function() {
+
+    }
+  };
+
+  var r, f, t;
   beforeEach(function(){
-    r = new ServiceResolver();
+    f = new Freight();
+    r = new ServiceResolver(f);
+    t = new Transit('/index');
   });
 
-  it("Construction", function(){
+  it("Construction, check interface", function(){
 
-    console.log('hey');
+    r.getArguments.should.be.an.instanceOf(Function);
+    r.getFunction.should.be.an.instanceOf(Function);    
+    r.getScope.should.be.an.instanceOf(Function);    
+    r.container.should.equal(f);
 
   });
+
+  it("get Scope", function(){
+
+    //default should use the original
+    var scope = r.getScope(t);
+    scope.should.equal(t);
+
+    t.setAttribute('_controller', 'controllers.default:index');
+
+    (function(){
+      scope = r.getScope(t);  //no controllers
+    }).should.throw();
+
+    f.register('controllers.cars', {
+      shared: "true",
+      factoryFn: function() {
+        return defaultController;
+      },
+      tags: ['controller']
+    });
+    
+    f.register('controllers.default', {
+      shared: "true",
+      factoryFn: function() {
+        return defaultController;
+      }
+    });
+
+    (function(){
+      scope = r.getScope(t); //not tagged
+    }).should.throw();
+
+
+    f.register('controllers.default', {
+      factoryFn: function() {
+        return defaultController;
+      },
+      tags: ['controller']
+    });
+
+    scope = r.getScope(t);
+    scope.should.equal(defaultController);
+
+  });
+
+
+  it("get function", function(){
+
+
+    var fn = r.getFunction(t);
+    fn.should.equal(false);
+
+    t.setAttribute('_controller', 'controllers.default:bogus');
+    f.register('controllers.default', {
+      factoryFn: function() {
+        return defaultController;
+      },
+      tags: ['controller']
+    });
+
+    (function(){
+        r.getFunction(t);
+    }).should.throw();
+
+    t.setAttribute('_controller', 'controllers.default:index');
+    fn = r.getFunction(t);
+    
+    fn.should.equal(defaultController.index);
+
+  });
+
 
 });
